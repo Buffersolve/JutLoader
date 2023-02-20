8dcd47d16bdb5b74505937b06457c07a36c84f59
Buffersolve <labish.vova@gmail.com>
2023-02-21 01:10:35 +0200

    repository unit testing

--- app/src/main/java/com/buffersolve/jutloader/data/repository/RepositoryImpl.kt
+++ app/src/main/java/com/buffersolve/jutloader/data/repository/RepositoryImpl.kt
@@ -24,6 +24,7 @@ class RepositoryImpl : Repository {
     override fun getOnlyOneSeriesUseCase(url: String, userAgent: String): Series {
         return GetOnlyOneSeasonSeries().execute(url, userAgent)
     }
+
     override fun isOnlyOneSeasonUseCase(url: String, userAgent: String): OneSeason {
         return IsOnlyOneSeasons().execute(url, userAgent)
     }
